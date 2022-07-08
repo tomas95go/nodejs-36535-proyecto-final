ab562ee455ce66937e519533bb24400e32b47f24
tomas95go <tomas95go@gmail.com>
2022-07-08 17:40:37 -0300

    getOne, addOne, updateOne y deleteOne de productos con Firebase

--- daos/products.dao.firebase.js
+++ daos/products.dao.firebase.js
@@ -20,6 +20,18 @@ async function getAll() {
   }
 }
 
+async function getOne(id) {
+  try {
+    const db = getFirebaseApp();
+
+    const product = db.collection("products").doc(id.toString()).get();
+
+    return product;
+  } catch (error) {
+    throw "Hubo un error al borrar el producto";
+  }
+}
+
 async function addOne(newProduct) {
   try {
     const db = getFirebaseApp();
@@ -36,7 +48,7 @@ async function addOne(newProduct) {
       timestamp,
     } = newProduct;
 
-    const productDocument = db.collection("products").doc(id);
+    const productDocument = db.collection("products").doc(id.toString());
 
     const addedProduct = await productDocument.set({
       id,
@@ -56,7 +68,49 @@ async function addOne(newProduct) {
   }
 }
 
+async function updateOne(id, newProduct) {
+  try {
+    const db = getFirebaseApp();
+
+    const { name, description, price, code, img, stock } = newProduct;
+
+    const productDocument = db.collection("products").doc(id.toString());
+
+    const updatedProduct = await productDocument.update({
+      name,
+      description,
+      price,
+      code,
+      img,
+      stock,
+    });
+
+    return updatedProduct;
+  } catch (error) {
+    throw "Hubo un error al actualizar el producto";
+  }
+}
+
+async function deleteOne(id) {
+  try {
+    const db = getFirebaseApp();
+
+    const productDocument = db.collection("products").doc(id.toString());
+
+    const updatedProduct = await productDocument.update({
+      active: false,
+    });
+
+    return updatedProduct;
+  } catch (error) {
+    throw "Hubo un error al borrar el producto";
+  }
+}
+
 module.exports = {
   getAll,
+  getOne,
   addOne,
+  updateOne,
+  deleteOne,
 };
