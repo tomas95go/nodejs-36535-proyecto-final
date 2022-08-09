6943b68878f7fe609dcd0429b567394d3f78ba5a
tomas95go <tomas95go@gmail.com>
2022-08-09 18:10:22 -0300

    Removiendo import de express-session. Separando la logica del server y la app

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+require("dotenv").config();
+
+const productsRouter = require(`${__dirname}/routes/products.route`);
+const cartsRouter = require(`${__dirname}/routes/carts.route`);
+const registerRouter = require(`${__dirname}/routes/register.route`);
+const loginRouter = require(`${__dirname}/routes/login.route`);
+const logoutRouter = require(`${__dirname}/routes/logout.route`);
+const routeHelper = require(`${__dirname}/helpers/route.helper`);
+const checkoutRouter = require(`${__dirname}/routes/checkout.route`);
+const profileRouter = require(`${__dirname}/routes/profile.route`);
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/register", registerRouter);
+app.use("/api/login", loginRouter);
+app.use("/api/logout", logoutRouter);
+app.use("/api/productos", productsRouter);
+app.use("/api/carrito", cartsRouter);
+app.use("/api/checkout", checkoutRouter);
+app.use("/api/profile", profileRouter);
+app.use(routeHelper.checkRoute);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,33 +1,11 @@
-const express = require("express");
 const path = require("path");
-const session = require("express-session");
 const http = require("http");
-require("dotenv").config();
 
-const productsRouter = require(`${__dirname}/routes/products.route`);
-const cartsRouter = require(`${__dirname}/routes/carts.route`);
-const registerRouter = require(`${__dirname}/routes/register.route`);
-const loginRouter = require(`${__dirname}/routes/login.route`);
-const logoutRouter = require(`${__dirname}/routes/logout.route`);
-const routeHelper = require(`${__dirname}/helpers/route.helper`);
-const checkoutRouter = require(`${__dirname}/routes/checkout.route`);
-const profileRouter = require(`${__dirname}/routes/profile.route`);
 const database = require(path.join(__dirname, "/config"));
+const app = require(path.join(__dirname, "/app"));
 
-const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8080;
-
-app.use(express.json());
-
-app.use("/api/register", registerRouter);
-app.use("/api/login", loginRouter);
-app.use("/api/logout", logoutRouter);
-app.use("/api/productos", productsRouter);
-app.use("/api/carrito", cartsRouter);
-app.use("/api/checkout", checkoutRouter);
-app.use("/api/profile", profileRouter);
-app.use(routeHelper.checkRoute);
 
 database.connect();
 
