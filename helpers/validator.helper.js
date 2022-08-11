92220f8747f72a6b713d4c5f6ecac7d273f49b14
tomas95go <tomas95go@gmail.com>
2022-08-11 13:57:28 -0300

    Mejorando el schema de Joi y desplegando los mensajes de errores correspondientes

--- helpers/validator.helper.js
+++ helpers/validator.helper.js
@@ -8,12 +8,13 @@ const userSchema = require(path.join(
 async function validateUserSchema(request, response, next) {
   try {
     const user = request.body;
-    const value = await userSchema.validateAsync(user, { abortEarly: false });
-    console.log(value);
+    await userSchema.validateAsync(user, { abortEarly: false });
+    next();
   } catch (error) {
+    const errors = error.details.map((detail) => detail.message);
     return response.status(400).json({
       message: "El formato de los datos no es correcto",
-      errors: error.details,
+      errors,
     });
   }
 }
