80c023f9f6e731390128d542b873bcd2fbccf48b
tomas95go <tomas95go@gmail.com>
2022-08-12 17:06:34 -0300

    Agrengando middleware de jwt al checkout

--- routes/checkout.route.js
+++ routes/checkout.route.js
@@ -2,12 +2,15 @@ const express = require("express");
 const path = require("path");
 
 const checkoutRouter = express.Router();
+
 const cartController = require(path.join(
   __dirname,
   "..",
   "/controllers/cart.controller"
 ));
 
-checkoutRouter.post("/:id_cart", cartController.checkout);
+const jwtHelper = require(path.join(__dirname, "..", `helpers/jwt.helper`));
+
+checkoutRouter.post("/:id_cart", jwtHelper.verify, cartController.checkout);
 
 module.exports = checkoutRouter;
