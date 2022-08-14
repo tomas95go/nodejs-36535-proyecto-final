317c9cb1093a84a1436fb9ba1f64eae3aa2139c6
tomas95go <tomas95go@gmail.com>
2022-08-14 19:46:24 -0300

    Actualizando las rutas del carrito

--- routes/carts.route.js
+++ routes/carts.route.js
@@ -9,6 +9,8 @@ const cartRouter = express.Router();
 cartRouter.use(jwtHelper.verify);
 
 cartRouter.get("/:id/productos", cartController.getAllProducts);
-cartRouter.post("/:id/productos", cartController.addManyProducts);
+cartRouter.post("/:id/productos", cartController.addOne);
+cartRouter.put("/:id/productos/:id_producto", cartController.updateOne);
+cartRouter.delete("/:id/productos/:id_producto", cartController.deleteOne);
 
 module.exports = cartRouter;
