aae718907df1213467c18bff9c0e09629fdf8bf2
tomas95go <tomas95go@gmail.com>
2022-08-16 12:39:03 -0300

    SocketIO JWT

---
+++ helpers/chat.helper.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+const chatController = require(path.join(
+  __dirname,
+  "..",
+  "controllers/chat.controller"
+));
+
+async function handleCustomerMessage(io, message) {
+  try {
+    console.log(`handleCustomerMessage: ${message}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function handleAdministratorMessage(io, message) {
+  try {
+    console.log(`handleCustomerMessage: ${message}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  handleCustomerMessage,
+  handleAdministratorMessage,
+};
